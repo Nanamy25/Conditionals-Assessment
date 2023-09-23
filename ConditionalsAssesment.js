b7afff88d45110f3bddcf64157281534773d535b
Annabell <annabellrambla@gmail.com>
2023-09-23 19:00:07 -0400

    Finished everything, it ALL works :>

--- ConditionalsAssesment.js
+++ ConditionalsAssesment.js
@@ -11,3 +11,14 @@ else {computerChoice = "Tails"}
 if (userChoice == computerChoice) 
 {alert ("You guessed right! The coin flip landed on " +userChoice)
 } else {alert ("Sorry, the coin flip landed on " +computerChoice)}
+
+var birthYear
+userChoice = prompt ("What year where you born in?")
+birthYear = userChoice
+if((2023 - birthYear) > 21) {
+    alert ("You are old enough to drink in the US")
+} else if ((2023 - birthYear) === 21) {
+    alert ("You are old enough to drink in the US...barely")
+} else if ((2023 - birthYear) < 21 ){
+    alert ("Sorry, you are not old enough to drink in the US")
+}
