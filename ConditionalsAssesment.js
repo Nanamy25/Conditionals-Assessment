79c57d92041d9929876c1533957779a291e10d1a
Annabell <annabellrambla@gmail.com>
2023-09-23 18:18:07 -0400

    Finally fixed the Heads or tale generator

--- ConditionalsAssesment.js
+++ ConditionalsAssesment.js
@@ -1,29 +1,13 @@
 var userChoice;
 var randomNumber;
 var computerChoice;
+
 userChoice = prompt("Heads or Tails");
 
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
-//   } console.log(getRandomInt(3));
-//    console.log(getRandomInt(1));
-//    console.log(Math.random());
+var randomNumber = Math.floor(Math.random()*2);
+ if (randomNumber == 1) {computerChoice = "Heads"}
+else {computerChoice = "Tails"}
 
-function randomNumber(){
-return Math.floor(Math.random()*2);
-}
-if (randomNumber === computerChoice) {
-    var computerChoice = "Tails";
-} else { 
-    var computerChoice = "Heads";
-}
-// Math.floor((Math.random() * 1) + 1);
-// var y = Math.random();
-// if (y < 0.5) {return  y = 0}
-//  else {y= 1};
-
-// let randomNumber = computerChoice
-// let (randomNumber = computerChoice);
 if (userChoice == computerChoice) 
 {alert ("You guessed right! The coin flip landed on " +userChoice)
 } else {alert ("Sorry, the coin flip landed on " +computerChoice)}
